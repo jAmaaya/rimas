cf916a25a079bc8816d8cb08f6de0f66c5f69144
jAmaaya <119801608+jAmaaya@users.noreply.github.com>
2022-12-05 14:04:52 -0400

    Ya hice todas las variables

--- rimas.js
+++ rimas.js
@@ -1,1 +1,13 @@
-const rimasAmor = ['Te miento si digo que no eres todo lo que pienso', 'se que esto comenzo en tan solo un momento']
+const rimasAmor = ['Te miento si digo que no eres todo lo que pienso',
+ 'se que esto comenzo en tan solo un momento', 
+ 'entre tantas personas solo a ti te espero, tal vez tu pienses que miento, nena si eres fuego me quemo no perdamos el tiempo', 
+ 'aquella noche en el bar, el primer dia que empezamos a hablar no imagine que iba a terminar enamorado, loco y sin pensar, en el daño que me hicieron antes ya no importaba porque estaba tu',
+ 'bailandito asi, pegaito mami, como me gusta a mi, enamorado baby, crazy, crazy asi'
+]
+const rimasDes = ['Yo se, que no fui el mejor, a pesar que me traicionaste el corazon, nena mirame sabes que ya no va mas, este juego que no va a continuar',
+'comenzo como lo mejor, termino jodiendo todo, lo que algun mi corazon sintio, todo fue solo un juego'
+]
+
+const rimasTris = ['no te puedo olvidar, eras todo lo que buscaba']
+
+
